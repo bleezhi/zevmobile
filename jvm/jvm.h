f93dc1eedf702b849385e482a55125cd10bafb22
blee <95976420+bleezhi@users.noreply.github.com>
2026-09-16 19:19:29 +0200

    Add JVM runtime interface

---
+++ jvm/jvm.h
@@ -0,0 +1,23 @@
+#ifndef ZEV_JVM_H
+#define ZEV_JVM_H
+
+#include <stddef.h>
+#include <stdint.h>
+#include "../emulator/phone.h"
+
+typedef struct {
+    const uint8_t *data;
+    size_t size;
+} ZevClassImage;
+
+typedef struct {
+    ZevPhone *phone;
+    int running;
+    int exit_code;
+} ZevJvm;
+
+int zev_jvm_init(ZevJvm *jvm, ZevPhone *phone);
+int zev_jvm_load_class(ZevJvm *jvm, ZevClassImage image);
+int zev_jvm_run_main(ZevJvm *jvm);
+
+#endif
