65a151bbf86fc2dd5f9c9e5f868abc81062dd339
blee <95976420+bleezhi@users.noreply.github.com>
2026-09-16 20:03:46 +0200

    jvm: expose loader diagnostics

--- jvm/jvm.h
+++ jvm/jvm.h
@@ -6,12 +6,13 @@
 #include "../emulator/phone.h"
 
 typedef struct { const uint8_t *data; size_t size; } ZevClassImage;
-typedef struct { ZevPhone *phone; int running; int exit_code; } ZevJvm;
+typedef struct { ZevPhone *phone; int running; int exit_code; int last_error; } ZevJvm;
 
 int zev_jvm_init(ZevJvm *jvm, ZevPhone *phone);
 int zev_jvm_load_class(ZevJvm *jvm, ZevClassImage image);
 int zev_jvm_run_main(ZevJvm *jvm);
 int zev_jvm_run_class(ZevJvm *jvm, ZevClassImage image);
+const char *zev_jvm_error_string(int error);
 extern const ZevClassImage zev_launcher_class;
 
 #endif
