12f5f093f046ae5c31bc065a5f0251c117636821
blee <95976420+bleezhi@users.noreply.github.com>
2026-09-16 19:20:52 +0200

    Add Java userspace launcher

---
+++ apps/Launcher.java
@@ -0,0 +1,7 @@
+public final class Launcher {
+    public static void main(String[] args) {
+        ZevMobile.boot();
+        ZevMobile.println("zevMobile JVM online");
+        ZevMobile.println("userspace launcher started");
+    }
+}
