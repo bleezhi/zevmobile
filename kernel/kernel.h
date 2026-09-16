fc1ad7e0438427fc875aaae80708830982353594
blee <95976420+bleezhi@users.noreply.github.com>
2026-09-16 19:05:16 +0200

    Expand kernel state for OS subsystems

--- kernel/kernel.h
+++ kernel/kernel.h
@@ -2,8 +2,19 @@
 #define ZEV_KERNEL_H
 
 #include "../emulator/phone.h"
+#include "fs.h"
+#include "process.h"
+#include "syscall.h"
+
+typedef struct {
+    ZevFs fs;
+    ZevScheduler scheduler;
+    ZevSyscallContext syscall_context;
+    int init_pid;
+} ZevKernel;
 
 void zev_kernel_boot(ZevPhone *phone);
 void zev_kernel_tick(ZevPhone *phone);
+const ZevKernel *zev_kernel_state(void);
 
 #endif
