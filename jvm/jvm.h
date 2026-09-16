fae3f99e0894e724a6613614b6cc2cb6453d6186
blee <95976420+bleezhi@users.noreply.github.com>
2026-09-16 19:20:45 +0200

    Expose JVM class runner

--- jvm/jvm.h
+++ jvm/jvm.h
@@ -5,19 +5,13 @@
 #include <stdint.h>
 #include "../emulator/phone.h"
 
-typedef struct {
-    const uint8_t *data;
-    size_t size;
-} ZevClassImage;
-
-typedef struct {
-    ZevPhone *phone;
-    int running;
-    int exit_code;
-} ZevJvm;
+typedef struct { const uint8_t *data; size_t size; } ZevClassImage;
+typedef struct { ZevPhone *phone; int running; int exit_code; } ZevJvm;
 
 int zev_jvm_init(ZevJvm *jvm, ZevPhone *phone);
 int zev_jvm_load_class(ZevJvm *jvm, ZevClassImage image);
 int zev_jvm_run_main(ZevJvm *jvm);
+int zev_jvm_run_class(ZevJvm *jvm, ZevClassImage image);
+extern const ZevClassImage zev_launcher_class;
 
 #endif
